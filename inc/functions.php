0c13d8dc72147cce50b806d6a15c7c50abb2e74f
Tales Henrique <webtailss@gmail.com>
2022-01-21 16:46:17 -0300

    Estrutura para funtions personalizado

---
+++ inc/functions.php
@@ -0,0 +1,9 @@
+<?php
+    
+  include_once('blocks/registers/block-about-us.php');
+  include_once('blocks/registers/block-gallery.php');
+  include_once('blocks/registers/block-repeater-content.php');
+
+  include_once('post_type/team.php');
+
+?>
