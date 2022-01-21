3e46ee45b0a4c2901f2856bc332f1950db50e17e
Tales Henrique <webtailss@gmail.com>
2022-01-21 16:37:10 -0300

    Head e Footer ajuste

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
-   <?php get_footer():?> 
+   <?php get_footer();?> 
 </body>
 </html>
--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title( '|', true, 'right' ); ?></title>
-    <?php get_head();?>
+    <?php wp_head();?>
 </head>
 <body <?php body_class();?> >
     
